a9eb89bb5574acdd203be9eda41816f76e7aec81
bdaniel7 <bdaniel7@gmail.com>
2013-10-29 20:23:13 +0200

    - just update

--- otis/app/scripts/controllers/tasks.js
+++ otis/app/scripts/controllers/tasks.js
@@ -1,16 +1,16 @@
 'use strict';
 
 angular.module('otisApp')
-  .controller('TasksController', function ($scope, $routeParams, Tasks) {
+  .controller('TasksCtrl', function ($scope, $routeParams, Tasks) {
     $scope.Tasks = Tasks;
     $scope.Task = Tasks[$routeParams.Id-1];
   })
-  .controller('StoriesController', function ($scope, $routeParams, Stories) {
+  .controller('StoriesCtrl', function ($scope, $routeParams, Stories) {
     $scope.Stories = Stories;
   })
 ;
 
 //angular.module('otisApp')
-//    .controller('TaskDetailsController', function ($scope, $routeParams, Tasks) {
+//    .controller('TaskDetailsCtrl', function ($scope, $routeParams, Tasks) {
 //        $scope.Task = $scope.Tasks[$routeParams.Id-1];
 //});
